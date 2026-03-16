9cb17ee3268c0178b3ab963ed7b58479abcb7f8f
Seongmin Oh <sminoh1205@gmail.com>
2026-03-16 20:30:00 +0900

    feat: add Asw_Manage module with LED control based on diagnostic results
    FAULT: LED OFF, WARNING: LED blink, NORMAL: LED ON.
    Reads CommFault, DataFault, EnvWarning from RteApp_Diag.

---
+++ STM32_Sensor_Monitor/App/ASW/Manage/Asw_Manage.c
@@ -0,0 +1,92 @@
+/*
+************************************************************************************************************************
+*
+* Filename        : Asw_Manage.c
+* Project         : STM32_Sensor_Monitor
+* Created         : 2026/03/27
+* Description     : ASW Manage Module Implementation.
+*                   Reads diagnostic results from RTE and controls status LED.
+*                   Priority: FAULT (LED OFF) > WARNING (LED blink) > NORMAL (LED ON).
+* Version         : 0.0.1
+* Author          : Seongmin Oh
+*
+************************************************************************************************************************
+*/
+/*
+************************************************************************************************************************
+*                                                  Include header files
+************************************************************************************************************************
+*/
+#include "Asw_Manage.h"
+#include "RteApp_Diag.h"
+#include "EcuAbs_Gpio.h"
+/*
+************************************************************************************************************************
+*                                                   Defines and macros
+************************************************************************************************************************
+*/
+
+/* Diagnostic result values (matches Asw_Diag output) */
+#define ASW_MANAGE_NO_FAULT     (0U)
+#define ASW_MANAGE_FAULT        (1U)
+#define ASW_MANAGE_WARNING      (2U)
+/*
+************************************************************************************************************************
+*                                                         Typedefs
+************************************************************************************************************************
+*/
+/*
+************************************************************************************************************************
+*                                                    Private variables
+************************************************************************************************************************
+*/
+/*
+************************************************************************************************************************
+*                                              Private function prototypes
+************************************************************************************************************************
+*/
+/*
+************************************************************************************************************************
+*                                                    Public functions
+************************************************************************************************************************
+*/
+
+/**
+ * @brief   Periodic manage processing.
+ *          Reads diagnostic results from RTE and controls status LED.
+ *          Priority: FAULT (LED OFF) > WARNING (LED blink) > NORMAL (LED ON).
+ * @retval  None
+ */
+void Asw_Manage_MainFunction(void)
+{
+    uint8_t commFault  = ASW_MANAGE_NO_FAULT;
+    uint8_t dataFault  = ASW_MANAGE_NO_FAULT;
+    uint8_t envWarning = ASW_MANAGE_NO_FAULT;
+
+    /* Read diagnostic results from RTE */
+    (void)RteApp_Diag_Read_CommFault(&commFault);
+    (void)RteApp_Diag_Read_DataFault(&dataFault);
+    (void)RteApp_Diag_Read_EnvWarning(&envWarning);
+
+    /* LED control: FAULT > WARNING > NORMAL */
+    if ((commFault == ASW_MANAGE_FAULT) || (dataFault == ASW_MANAGE_FAULT))
+    {
+        /* FAULT: LED OFF */
+        EcuAbs_Gpio_ClearLed();
+    }
+    else if (envWarning == ASW_MANAGE_WARNING)
+    {
+        /* WARNING: LED blink (toggle each cycle) */
+        EcuAbs_Gpio_ToggleLed();
+    }
+    else
+    {
+        /* NORMAL: LED ON */
+        EcuAbs_Gpio_SetLed();
+    }
+}
+/*
+************************************************************************************************************************
+*                                                   Private functions
+************************************************************************************************************************
+*/
